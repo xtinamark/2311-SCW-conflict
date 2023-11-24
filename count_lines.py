8fdf1ff1c9ec63b7b60c1dc35ebd931b3aadd410
cmarkanastasak <c.markanastasakis@tudelft.nl>
2023-11-24 11:45:19 +0100

    hell

--- count_lines.py
+++ count_lines.py
@@ -2,10 +2,15 @@
 import sys
 
 """"
-Christina Hello
+This script counts the lines in standard input
+Input: text from the system.
 """"
 count =0
 for line in sys.stdin:
 	count +=1
 
 print(count, "lines in standard input")
+
+Add explanation at the top
+
+I think it is good practice to document a script. 
